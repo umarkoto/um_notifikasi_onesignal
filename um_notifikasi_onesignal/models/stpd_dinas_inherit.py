065f7e245ea923abbcebae8668284e11ef76d18a
Umar <umarkoto@gmail.com>
2023-10-21 00:16:34 +0700

    Update stpd_dinas_inherit.py
    ganti create employee

--- um_notifikasi_onesignal/models/stpd_dinas_inherit.py
+++ um_notifikasi_onesignal/models/stpd_dinas_inherit.py
@@ -1,14 +1,27 @@
 from odoo import api, models
 
-class StpdDinasInherit(models.Model):
-    _inherit = 'stpd.dinas'
+# class StpdDinasInherit(models.Model):
+#     _inherit = 'stpd.dinas'
+
+#     @api.model
+#     def create(self, vals):
+#         record = super(StpdDinasInherit, self).create(vals)
+
+#         # Kirim notifikasi dengan OneSignal setelah record berhasil dibuat
+#         message = "Pengajuan dinas baru dengan Nomor: {} telah berhasil dibuat.".format(record.name)
+#         self.env['onesignal.notification'].send_notification(message)
+
+#         return record
+
+class HrEmployeeInherit(models.Model):
+    _inherit = 'hr.employee'
 
     @api.model
     def create(self, vals):
-        record = super(StpdDinasInherit, self).create(vals)
+        record = super(HrEmployeeInherit, self).create(vals)
 
-        # Kirim notifikasi dengan OneSignal setelah record berhasil dibuat
-        message = "Pengajuan dinas baru dengan Nomor: {} telah berhasil dibuat.".format(record.name)
+        # Send notification with OneSignal after employee record is successfully created
+        message = "A new employee named {} has been successfully added.".format(record.name)
         self.env['onesignal.notification'].send_notification(message)
 
         return record
