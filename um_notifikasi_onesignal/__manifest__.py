a8846186c38e83c7ea3233e008f1d0ac35c752fe
Umar <umarkoto@gmail.com>
2024-10-23 10:21:26 +0700

    Update __manifest__.py
    test

--- um_notifikasi_onesignal/__manifest__.py
+++ um_notifikasi_onesignal/__manifest__.py
@@ -9,7 +9,7 @@
         OneSignal connector
     """,
 
-    'currency': 'IDR',
+    'currency': 'EUR',
     'price': 40,
 
     'author': "Umar",
@@ -20,7 +20,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tools',
-    'version': '17.0.1.0.1',
+    'version': '16.0.1.0.1',
     'images': ['static/description/banner.gif'],
     # any module necessary for this one to work correctly
     'depends': ['base', 'um_stpd', 'web'],
